bc5f842232f768f1f9873bb38c0974c4fe0ada04
Tony Greenberg <info@bayesicresearch.org>
2023-01-10 19:58:42 -0500

    start on the _tzcnt() based hash OPH calculation

--- tests/hashTests.cpp
+++ tests/hashTests.cpp
@@ -27,6 +27,7 @@
 #include <array>
 #include <bitset>
 #include <immintrin.h>
+#include <x86intrin.h>
 
 #include "random.hpp"
 
@@ -61,7 +62,7 @@ uint32_t murMurHash(const size_t &key, const uint32_t &seed) {
 	return hash;
 }
 
-std::array<float, 2> locusOPH(const size_t &locusInd, const size_t &nIndividuals, const size_t &locusSize, const size_t &kSketches, const size_t &sketchSize,
+std::array<float, 2> locusOPHold(const size_t &locusInd, const size_t &nIndividuals, const size_t &locusSize, const size_t &kSketches, const size_t &sketchSize,
 	const std::vector<size_t> &permutation, std::vector<uint32_t> &seeds, BayesicSpace::RanDraw &rng, std::vector<uint8_t> &binLocus, std::vector<uint16_t> &sketches) {
 	std::chrono::duration<float, std::milli> permTime{0};
 	std::chrono::duration<float, std::milli> sketchTime{0};
@@ -70,7 +71,7 @@ std::array<float, 2> locusOPH(const size_t &locusInd, const size_t &nIndividuals
 	constexpr uint8_t byteSize       = 8;
 	constexpr uint16_t oneBit        = 1;
 	constexpr uint64_t roundMask     = 0xfffffffffffffff8;
-	//const uint16_t emptyBinToken = std::numeric_limits<uint16_t>::max();
+	const uint16_t emptyBinToken = std::numeric_limits<uint16_t>::max();
 	// Calculate the number of full bytes; nIndividuals - 1 because Fisher-Yates goes up to N - 2 inclusive
 	const size_t nFullBytes = (nIndividuals - 1) / byteSize;
 	size_t iIndiv           = 0;
@@ -121,11 +122,10 @@ std::array<float, 2> locusOPH(const size_t &locusInd, const size_t &nIndividuals
 	}
 	auto time2 = std::chrono::high_resolution_clock::now();
 	permTime   = time2 - time1;
-	/*
 	// Now make the sketches
 	time1 = std::chrono::high_resolution_clock::now();
 	std::vector<size_t> filledIndexes;                // indexes of the non-empty sketches
-	size_t iByte     = 0;
+	iByte            = 0;
 	size_t colEnd    = iByte + locusSize;
 	size_t sketchBeg = locusInd * kSketches;
 	size_t iSeed     = 0;                             // index into the seed vector
@@ -138,7 +138,7 @@ std::array<float, 2> locusOPH(const size_t &locusInd, const size_t &nIndividuals
 				(firstSetBitPos < sketchSize) ){
 			++iInByte;
 			// these are instead of an if statement
-			iByte  += iInByte == byteSize;
+			iByte  += static_cast<size_t>(iInByte == byteSize);
 			iInByte = iInByte % byteSize;
 			++firstSetBitPos;
 		}
@@ -149,9 +149,9 @@ std::array<float, 2> locusOPH(const size_t &locusInd, const size_t &nIndividuals
 				sketches[sketchBeg + iSketch] = firstSetBitPos;
 			}
 
-			uint16_t remainder = sketchSize - firstSetBitPos;
-			uint16_t inByteMod = remainder % byteSize;
-			uint16_t inByteSum = iInByte + inByteMod;
+			const auto remainder     = static_cast<uint16_t>(sketchSize - firstSetBitPos);
+			const uint16_t inByteMod = remainder % byteSize;
+			const uint16_t inByteSum = iInByte + inByteMod;
 
 			iByte  += remainder / byteSize + inByteSum / byteSize;
 			iInByte = inByteSum % byteSize;
@@ -184,13 +184,163 @@ std::array<float, 2> locusOPH(const size_t &locusInd, const size_t &nIndividuals
 		}
 	}
 	time2      = std::chrono::high_resolution_clock::now();
-	*/
+	sketchTime = time2 - time1;
+	return std::array<float, 2>{permTime.count(), sketchTime.count()};
+}
+
+std::array<float, 2> locusOPH(const size_t &locusInd, const size_t &nIndividuals, const size_t &locusSize, const size_t &kSketches, const size_t &sketchSize,
+	const std::vector<size_t> &permutation, std::vector<uint32_t> &seeds, BayesicSpace::RanDraw &rng, std::vector<uint8_t> &binLocus, std::vector<uint16_t> &sketches) {
+	std::chrono::duration<float, std::milli> permTime{0};
+	std::chrono::duration<float, std::milli> sketchTime{0};
+	// Start with a permutation to make OPH
+	auto time1                      = std::chrono::high_resolution_clock::now();
+	constexpr uint8_t byteSize      = 8;
+	constexpr size_t wordSize       = 8;
+	constexpr uint16_t oneBit       = 1;
+	constexpr uint64_t roundMask    = 0xfffffffffffffff8;
+	constexpr size_t wordSizeInBits = 64;
+	// Calculate the number of full bytes; nIndividuals - 1 because Fisher-Yates goes up to N - 2 inclusive
+	const size_t nFullBytes = (nIndividuals - 1) / byteSize;
+	size_t iIndiv           = 0;
+	size_t iByte            = 0;
+	while(iByte < nFullBytes){
+		for (uint8_t iInLocusByte = 0; iInLocusByte < byteSize; ++iInLocusByte){
+			auto bytePair            = static_cast<uint16_t>(binLocus[iByte]);
+			const size_t perIndiv    = permutation[iIndiv++];                                   // post-increment to use current value for index first
+			const size_t permByteInd = perIndiv / byteSize;
+			const auto permInByteInd = static_cast<uint8_t>( perIndiv - (perIndiv & roundMask) );
+			// Pair the current locus byte with the byte containing the value to be swapped
+			// Then use the exchanging two fields trick from Hacker's Delight Chapter 2-20
+			bytePair                    |= static_cast<uint16_t>(binLocus[permByteInd]) << byteSize;
+			const auto mask              = static_cast<uint16_t>(oneBit << iInLocusByte);
+			const auto perMask           = static_cast<uint8_t>(oneBit << permInByteInd);
+			const uint16_t shiftDistance = (byteSize - iInLocusByte) + permInByteInd;           // subtraction is safe b/c byteSize is the loop terminator
+			const uint16_t temp1         = ( bytePair ^ (bytePair >> shiftDistance) ) & mask;
+			const auto temp2             = static_cast<uint16_t>(temp1 << shiftDistance);
+			bytePair                    ^= temp1 ^ temp2;
+			// Transfer bits using the trick in Hacker's Delight Chapter 2-20 (do not need the full swap, just transfer from the byte pair to binLocus)
+			// Must modify the current byte in each loop iteration because permutation indexes may fall into it
+			binLocus[iByte]             ^= ( binLocus[iByte] ^ static_cast<uint8_t>(bytePair) ) & static_cast<uint8_t>(mask);
+			binLocus[permByteInd]       ^= ( binLocus[permByteInd] ^ static_cast<uint8_t>(bytePair >> byteSize) ) & perMask;
+ 		}
+		++iByte;
+	}
+	// Finish the individuals in the remaining partial byte, if any
+	uint8_t iInLocusByte = 0;
+	while (iIndiv < nIndividuals - 1){
+		auto bytePair            = static_cast<uint16_t>(binLocus[iByte]);
+		const size_t perIndiv    = permutation[iIndiv++];                                   // post-increment to use current value for index first
+		const size_t permByteInd = perIndiv / byteSize;
+		const auto permInByteInd = static_cast<uint8_t>( perIndiv - (perIndiv & roundMask) );
+		// Pair the current locus byte with the byte containing the value to be swapped
+		// Then use the exchanging two fields trick from Hacker's Delight Chapter 2-20
+		bytePair                    |= static_cast<uint16_t>(binLocus[permByteInd]) << byteSize;
+		const auto mask              = static_cast<uint16_t>(oneBit << iInLocusByte);
+		const auto perMask           = static_cast<uint8_t>(oneBit << permInByteInd);
+		const uint16_t shiftDistance = (byteSize - iInLocusByte) + permInByteInd;           // subtraction is safe b/c byteSize is the loop terminator
+		const uint16_t temp1         = ( bytePair ^ (bytePair >> shiftDistance) ) & mask;
+		const auto temp2             = static_cast<uint16_t>(temp1 << shiftDistance);
+		bytePair                    ^= temp1 ^ temp2;
+		// Transfer bits using the trick in Hacker's Delight Chapter 2-20 (do not need the full swap, just transfer from the byte pair to binLocus)
+		// Must modify the current byte in each loop iteration because permutation indexes may fall into it
+		binLocus[iByte]             ^= ( binLocus[iByte] ^ static_cast<uint8_t>(bytePair) ) & static_cast<uint8_t>(mask);
+		binLocus[permByteInd]       ^= ( binLocus[permByteInd] ^ static_cast<uint8_t>(bytePair >> byteSize) ) & perMask;
+		++iInLocusByte;
+	}
+	auto time2 = std::chrono::high_resolution_clock::now();
+	permTime   = time2 - time1;
+	// Now make the sketches
+	time1 = std::chrono::high_resolution_clock::now();
+	constexpr uint16_t emptyBinToken = std::numeric_limits<uint16_t>::max();
+	const size_t nBytesToHash        = (kSketches * sketchSize) / byteSize;
+	const size_t nEvenBytesToHash    = nBytesToHash & roundMask;
+	std::cout << "nBytesToHash = " << nBytesToHash << "; nEvenBytesToHash = " << nEvenBytesToHash << "\n";
+	std::vector<size_t> filledIndexes;                                                 // indexes of the non-empty sketches
+	iByte            = 0;
+	size_t sketchBeg = locusInd * kSketches;
+	size_t iSeed     = 0;                                                              // index into the seed vector
+	size_t iSketch   = 0;
+	while (iByte < nEvenBytesToHash){
+		uint64_t locusChunk = 0;
+		memcpy(&locusChunk, binLocus.data(), wordSize);
+		// TODO: add a cassert() for iSketch * sketchSize > wordSizeInBits
+		//  FIX!!!
+		const uint64_t initialShift = iSketch * sketchSize - wordSizeInBits;           // in case there is a trailing part of the previous sketch
+		std::cout << "X; " << std::bitset<byteSize * wordSize>(locusChunk) << "\n";
+		locusChunk                  = locusChunk >> initialShift;
+		std::cout << "X; " << std::bitset<byteSize * wordSize>(locusChunk) << "\n";
+		while (locusChunk != 0){
+			const uint64_t setBit = _tzcnt_u64(locusChunk);                           // trailing zero count counts from the correct end
+			std::cout << setBit << "; ";
+			const uint64_t idxToSkip = setBit / sketchSize;
+			const uint64_t iShift    = sketchSize * (idxToSkip + 1);
+			locusChunk               = locusChunk >> iShift;
+			iSketch                 += idxToSkip;
+			filledIndexes.push_back(iSketch);
+			sketches[sketchBeg + iSketch] = static_cast<uint16_t>(setBit);             // should be safe: each thread accesses different vector elements
+			++iSketch;
+			std::cout << std::bitset<byteSize * wordSize>(locusChunk) << "\n";
+		}
+		std::cout << "===========\n";
+		iByte += wordSize;
+	}
+	if (iByte < nBytesToHash){
+		uint64_t locusChunk{0};
+		memcpy(&locusChunk, binLocus.data(), nBytesToHash - iByte);
+		// TODO: add a cassert() for iSketch * sketchSize > wordSizeInBits
+		const uint64_t initialShift = iSketch * sketchSize - wordSizeInBits;           // in case there is a trailing part of the previous sketch
+		std::cout << "X; " << std::bitset<byteSize * wordSize>(locusChunk) << "\n";
+		locusChunk                  = locusChunk >> initialShift;
+		std::cout << "0; " << std::bitset<byteSize * wordSize>(locusChunk) << "\n";
+		while (locusChunk != 0){
+			const uint64_t setBit = _tzcnt_u64(locusChunk);                            // trailing zero count counts from the correct end
+			std::cout << setBit << "; ";
+			const uint64_t idxToSkip = setBit / sketchSize;
+			const uint64_t iShift    = sketchSize * (idxToSkip + 1);
+			locusChunk               = locusChunk >> iShift;
+			iSketch                 += idxToSkip;
+			filledIndexes.push_back(iSketch);
+			sketches[sketchBeg + iSketch] = static_cast<uint16_t>(setBit);             // should be safe: each thread accesses different vector elements
+			++iSketch;
+			std::cout << std::bitset<byteSize * wordSize>(locusChunk) << "\n";
+		}
+	}
+	if (filledIndexes.size() == 1){
+		for (size_t iSk = 0; iSk < kSketches; ++iSk){ // this will overwrite the one assigned sketch, but the wasted operation should be swamped by the rest
+			// should be safe: each thread accesses different vector elements
+			sketches[sketchBeg + iSk] = sketches[filledIndexes[0] + sketchBeg];
+		}
+	} else if (filledIndexes.size() != kSketches){
+		if ( filledIndexes.empty() ){ // in the case where the whole locus is monomorphic, pick a random index as filled
+			filledIndexes.push_back( rng.sampleInt(kSketches) );
+		}
+		size_t emptyCount = kSketches - filledIndexes.size();
+		while (emptyCount > 0){
+			for (const auto &eachIdx : filledIndexes){
+				size_t newIdx = murMurHash(eachIdx, seeds[iSeed]) % kSketches + sketchBeg;
+				// should be safe: each thread accesses different vector elements
+				if (sketches[newIdx] == emptyBinToken){
+					sketches[newIdx] = sketches[eachIdx + sketchBeg];
+					--emptyCount;
+					break;
+				}
+			}
+			++iSeed;
+			if ( iSeed == seeds.size() ){
+				seeds.push_back( static_cast<uint32_t>( rng.ranInt() ) );
+			}
+		}
+	}
+	time2      = std::chrono::high_resolution_clock::now();
 	sketchTime = time2 - time1;
 	return std::array<float, 2>{permTime.count(), sketchTime.count()};
 }
 
 int main() {
-	BayesicSpace::RanDraw prng;
+	BayesicSpace::RanDraw seedPRNG;
+	const uint64_t seedInt  = seedPRNG.ranInt();
+	BayesicSpace::RanDraw prng1(seedInt);
+	BayesicSpace::RanDraw prng2(seedInt);
 	constexpr size_t wordSizeBits    = 64;
 	constexpr size_t wordSize        = 8;
 	constexpr size_t byteSize        = 8;
@@ -200,44 +350,48 @@ int main() {
 	//constexpr size_t kSketches       = 100;
 	constexpr size_t kSketches       = 20;
 	constexpr size_t locusSize       = nIndividuals / byteSize + static_cast<size_t>( static_cast<bool>(nIndividuals % byteSize) );
-	constexpr size_t sketchSize      = nIndividuals / kSketches + static_cast<size_t>( static_cast<bool>(nIndividuals % kSketches) );
+	constexpr size_t sketchSize      = nIndividuals / kSketches;
 	constexpr uint16_t emptyBinToken = std::numeric_limits<uint16_t>::max();
 	constexpr size_t ranBitVecSize   = nIndividuals / wordSizeBits + static_cast<size_t>( static_cast<bool> (nIndividuals % wordSizeBits) );
-	std::vector<uint32_t> seeds{static_cast<uint32_t>( prng.ranInt() )};
-	std::vector<uint16_t> sketches(kSketches, emptyBinToken);
-	std::vector<uint8_t> binLocus;
+	std::vector<uint32_t> seeds1{static_cast<uint32_t>( prng1.ranInt() )};
+	std::vector<uint32_t> seeds2{static_cast<uint32_t>( prng2.ranInt() )};
+	std::vector<uint16_t> sketches1(kSketches, emptyBinToken);
+	std::vector<uint16_t> sketches2(kSketches, emptyBinToken);
+	std::vector<uint8_t> binLocus1;
+	std::vector<uint8_t> binLocus2;
 	std::vector<uint64_t> ranBits;
 	for (size_t i = 0; i < ranBitVecSize; ++i){
-		ranBits.push_back( prng.ranInt() );
+		ranBits.push_back( seedPRNG.ranInt() );
 	}
 	size_t iByte = 0;
-	binLocus.resize(locusSize);
+	binLocus1.resize(locusSize);
 	for (const auto eachWord : ranBits){
 		for (size_t byteInWord = 0; (byteInWord < wordSize) && (iByte < locusSize); ++byteInWord){
-			binLocus[iByte] = static_cast<uint8_t>( eachWord >> (byteInWord * byteSize) );
+			binLocus1[iByte] = static_cast<uint8_t>( eachWord >> (byteInWord * byteSize) );
 			++iByte;
 		}
 	}
-	
-	for (auto blIt = binLocus.rbegin(); blIt != binLocus.rend(); ++blIt){
+	binLocus2 = binLocus1;
+	for (auto blIt = binLocus1.rbegin(); blIt != binLocus1.rend(); ++blIt){
 		std::cout << std::bitset<byteSize>(*blIt);
 	}
 	std::cout << "\n";
 	//const size_t remainder = (nIndividuals % byteSize ? byteSize - nIndividuals % byteSize : 0);
 	//binLocus.back() &= std::numeric_limits<uint8_t>::max() >> remainder;
-	constexpr size_t nCycles = 1000000;
-	for (size_t i = 0; i < nCycles; ++i){
-		std::vector<size_t> ranIntsUp{prng.fyIndexesUp(nIndividuals)};
-		std::vector<uint8_t> locLocus = binLocus;
-		const std::array<float, 2> res2 = locusOPH(0, nIndividuals, locusSize, kSketches, sketchSize, ranIntsUp, seeds, prng, locLocus, sketches);
-		for (auto blIt = locLocus.rbegin(); blIt != locLocus.rend(); ++blIt){
-			std::cout << std::bitset<byteSize>(*blIt);
-		}
-		std::cout << "\n";
+	std::vector<size_t> ranIntsUp{seedPRNG.fyIndexesUp(nIndividuals)};
+	const std::array<float, 2> res1 = locusOPHold(0, nIndividuals, locusSize, kSketches, sketchSize, ranIntsUp, seeds1, prng1, binLocus1, sketches1);
+	const std::array<float, 2> res2 = locusOPH(0, nIndividuals, locusSize, kSketches, sketchSize, ranIntsUp, seeds2, prng2, binLocus2, sketches2);
+	for (auto blIt = binLocus1.rbegin(); blIt != binLocus1.rend(); ++blIt){
+		std::cout << std::bitset<byteSize>(*blIt);
 	}
-	//std::cout << std::bitset<nIndividuals % byteSize>(*blIt);
-	//++blIt;
-	/*
+	std::cout << "\n";
+	for (const auto eachSketch1 : sketches1){
+		std::cout << eachSketch1 << " ";
+	}
+	std::cout << "\n";
+	for (const auto eachSketch2 : sketches2){
+		std::cout << eachSketch2 << " ";
+	}
+	std::cout << "\n";
 	std::cout << res1[0] << "\t" << res1[1] << "\t" << res2[0] << "\t" << res2[1] << "\n";
-	*/
 }
